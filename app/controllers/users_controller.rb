1f8cb6e2d2a4769f22b5f9ad72d79230f512c528
William Kasel <wkasel@gmail.com>
2017-02-05 19:47:17 -0800

    move authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  after_action :verify_authorized, except: [:show]
+  after_action :verify_authorized
 
   def index
     @users = User.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!([
   {email: "megan@eatgreenbox.com", password: "100Green", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2014-12-30 23:41:54", last_sign_in_at: "2014-12-30 23:40:40", current_sign_in_ip: "::1", last_sign_in_ip: "::1", role: 2},
-  {email: "megan@eatgreenbox.com", password: "100Green", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2014-12-30 23:41:54", last_sign_in_at: "2014-12-30 23:40:40", current_sign_in_ip: "::1", last_sign_in_ip: "::1", role: 2},
+  {email: "william@eatgreenbox.com", password: "100Green", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 4, current_sign_in_at: "2014-12-30 23:41:54", last_sign_in_at: "2014-12-30 23:40:40", current_sign_in_ip: "::1", last_sign_in_ip: "::1", role: 2},
   {email: "team@eatgreenbox.com", password: "gb2017", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2014-03-10 11:28:22", last_sign_in_at: "2014-03-10 11:28:22", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1", role: 0}
 ])
